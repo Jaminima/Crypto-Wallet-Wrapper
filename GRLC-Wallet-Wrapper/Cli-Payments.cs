7f227e13740cb761ee97c8ca8e9ec9be67e7f6bd
Oscar Davies <oscar.william.davies@outlook.com>
2021-05-31 12:41:31 +0100

    Confimation Tracking in seperate File

--- GRLC-Wallet-Wrapper/Cli-Payments.cs
+++ GRLC-Wallet-Wrapper/Cli-Payments.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,50 +11,16 @@ namespace GRLC_Wallet_Wrapper
             return await Cli_Manager.DoAndReadClientRequest(new string[] { "sendtoaddress", address, amount.ToString(), "Pay Out", "Customer", deductFees.ToString() });
         }
 
-        private static HashSet<string> confirmedTxIds = ReadConfimed();
-
-        public static HashSet<string> ReadConfimed()
-        {
-            HashSet<string> set = new HashSet<string>();
-
-            if (File.Exists("./confirmedTxIds.txt"))
-            {
-                StreamReader reader = new StreamReader(new FileStream("./confirmedTxIds.txt", FileMode.Open));
-                string s = reader.ReadToEnd();
-
-                foreach (string line in s.Split("\n")) set.Add(line);
-            }
-            return set;
-        }
-
-        public static void StoreConfimed()
-        {
-            StreamWriter writer = new StreamWriter(new FileStream("./confirmedTxIds.txt", FileMode.CreateNew));
-            writer.Write(string.Join("\n", confirmedTxIds));
-            writer.Flush();
-            writer.Close();
-        }
-
-        public static void AppendConfirmed(string txId)
-        {
-            confirmedTxIds.Add(txId);
-
-            StreamWriter writer = new StreamWriter(new FileStream("./confirmedTxIds.txt", FileMode.Append));
-            writer.Write(txId);
-            writer.Flush();
-            writer.Close();
-        }
-
         public static async Task<Objects.Account> ConfirmPayment(string receiveAddress, string txId)
         {
-            if (confirmedTxIds.Contains(txId)) return null;
+            if (ConfirmedTxs.AlreadyConfirmed(txId)) return null;
 
             Objects.Transaction t = await Cli_Gets.GetTransaction(txId);
 
             IEnumerable<Objects.Account> f = t.details.Where(x => x.category == "receive" && x.address == receiveAddress);
             if (f.Any())
             {
-                AppendConfirmed(txId);
+                ConfirmedTxs.AppendConfirmed(txId);
                 return f.First();
             }
             else return null;
---
+++ GRLC-Wallet-Wrapper/ConfirmedTxs.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace GRLC_Wallet_Wrapper
+{
+    public static class ConfirmedTxs
+    {
+        private static HashSet<string> confirmedTxIds = ReadConfimed();
+
+        public static bool AlreadyConfirmed(string txId)
+        {
+            return confirmedTxIds.Contains(txId);
+        }
+
+        private static HashSet<string> ReadConfimed()
+        {
+            HashSet<string> set = new HashSet<string>();
+
+            if (File.Exists("./confirmedTxIds.txt"))
+            {
+                StreamReader reader = new StreamReader(new FileStream("./confirmedTxIds.txt", FileMode.Open));
+                string s = reader.ReadToEnd();
+
+                foreach (string line in s.Split("\n")) set.Add(line);
+            }
+            return set;
+        }
+
+        public static void StoreConfimed()
+        {
+            StreamWriter writer = new StreamWriter(new FileStream("./confirmedTxIds.txt", FileMode.CreateNew));
+            writer.Write(string.Join("\n", confirmedTxIds));
+            writer.Flush();
+            writer.Close();
+        }
+
+        public static void AppendConfirmed(string txId)
+        {
+            confirmedTxIds.Add(txId);
+
+            StreamWriter writer = new StreamWriter(new FileStream("./confirmedTxIds.txt", FileMode.Append));
+            writer.Write(txId);
+            writer.Flush();
+            writer.Close();
+        }
+    }
+}
